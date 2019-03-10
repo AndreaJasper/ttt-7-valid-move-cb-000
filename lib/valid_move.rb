c69a7d826cd6019d7846ef63fe91e3966a8f523e
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2019-03-10 04:08:52 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -16,7 +16,7 @@ def valid_move?(board, index)
   end
 
   if (position_taken?(board, index)) == false && (on_board?(index) == true)
-     return true
+    return true
   else
     return false
   end
